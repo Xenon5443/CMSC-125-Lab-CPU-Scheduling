a71a059dfd20d5c2c4fad5182a408bd3c7647b31
Xenon5443 <mtmisola@up.edu.ph>
2026-05-22 00:08:14 +0800

    feat: Added to do place holders for MLFQ

---
+++ schedsim/src/mlfq.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <limits.h>
+
+#include "process.h"
+#include "scheduler.h"
+#include "gantt.h"
+#include "state.h"
+#include "mlfq_config.h"
+
+int schedule_mlfq(SchedulerState *state, MLFQConfig *config){
+    return 0;
+}
