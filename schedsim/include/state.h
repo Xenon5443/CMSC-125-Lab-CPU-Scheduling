f98e99950149de32a09b1bc95ccbc810b12a6f01
Xenon5443 <mtmisola@up.edu.ph>
2026-05-22 00:07:30 +0800

    feat: Added state as the central structure

---
+++ schedsim/include/state.h
@@ -0,0 +1,27 @@
+#ifndef STATE_H
+#define STATE_H
+
+#include <stdbool.h>
+
+#include "process.h"
+#include "mlfq_config.h"
+
+typedef struct ProcessQueue ProcessQueue;
+
+
+typedef struct {
+    Process *processes;
+    int num_processes;
+    int current_time;
+
+    ProcessQueue *process_queue;
+    bool run_state;
+} SchedulerState;
+
+int schedule_fcfs(SchedulerState *state);
+int schedule_sjf(SchedulerState *state);
+int schedule_stcf(SchedulerState *state);
+int schedule_rr(SchedulerState *state, int quantum);
+int schedule_mlfq(SchedulerState *state, MLFQConfig *config);
+
+#endif
